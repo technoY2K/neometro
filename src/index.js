76286cc8020f4594937dab6efb01ac02840effd2
jhamPac <cloud@kevia.me>
2019-07-07 20:54:38 -0700

    basic cli setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log('hello world')
+require('yargs')
+  .scriptName("neometro")
+  .usage('$0 <cmd> [args]')
+  .command('hello [name]', 'welcome to neometro',
+  (yargs) => {
+    yargs.positional('name', {
+      type: 'string',
+      default: 'User',
+      describe: 'the name to say hello to'
+    })
+  },
+  (argv) => {
+    console.log('hello', argv.name, 'welcome to neometro a cli for neocities!')
+  })
+  .help()
+  .argv
