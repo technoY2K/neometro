4ec6c1abacdd6eb854621343e584ed1b643fdcfc
jhamPac <cloud@kevia.me>
2019-07-21 14:14:32 -0700

    checking if prettier is doing its thing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import 'regenerator-runtime/runtime'
 import list from './list.js'
 
-require('yargs')
-  .scriptName('neometro')
-  .usage('$0 <cmd> [args]')
-  .command('list', 'list files in root directory', list.builder, list.handler)
-  .help().argv
+require('yargs').scriptName('neometro').usage('$0 <cmd> [args]').command('list', 'list files in root directory', list.builder, list.handler).help().argv
