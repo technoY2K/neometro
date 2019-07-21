9d91e55f916b6e891f2d815ee6a3bc52958b836d
jhamPac <cloud@kevia.me>
2019-07-21 11:50:16 -0700

    dotenv needed a path for the env file

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-
-const Conf = require('conf');
+require('dotenv').config({path: '../.env'})
+const Conf = require('conf')
 const packageJson = require('../package.json')
 
 const config = new Conf({configName: packageJson.name})
+
+
+config.set('siteName', `${process.env.SITE}`)
+config.set('password', `${process.env.PASSWORD}`)
