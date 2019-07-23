87d0d358682b4ee3f17dd168e98cde7ba9363e35
jhamPac <cloud@kevia.me>
2019-07-22 21:29:27 -0700

    trying to figure out how to set default commands

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import 'regenerator-runtime/runtime'
 import list from './list.js'
+const yargs = require('yargs')
 
-require('yargs')
+yargs
   .scriptName('neometro')
   .usage('$0 <cmd> [args]')
+  .command('$0', 'Welcome to NeoMetro, your gateway to NeoCities', () => {}, (argv) => {
+    yargs.showHelp()
+  })
+  .demandCommand(1, 'You need at least one command before moving on')
   .command('list', 'list files in root directory', list.builder, list.handler)
-  .help().argv
+  .help()
+  .argv
